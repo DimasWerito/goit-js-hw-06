bf775b99d7476aa009750da6a3680fbad4f27b5d
Dmytro Krasilnykov <krasilnykov@gmail.com>
2024-09-16 22:32:13 +0300

    task 3

--- js/task-2.js
+++ js/task-2.js
@@ -17,3 +17,12 @@ class Storage {
     this.#items = this.#items.filter(item => item !== itemToRemove);
   }
 }
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
